7e2f6a77d796b3690307143df888e24f45643776
lbchao <783113203@qq.com>
2015-12-10 20:59:08 +0800

    update

---
+++ app/src/main/java/com/example/syl/mybugapplication/entity/castEntity.java
@@ -0,0 +1,29 @@
+package com.example.syl.mybugapplication.entity;
+
+/**
+ * Created by lin on 2015/12/6.
+ */
+public class CastEntity {
+
+    private String  name;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String  desc;
+
+
+}
